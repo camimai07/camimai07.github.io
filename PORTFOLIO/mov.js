2c2dee542c9fe7e83d3f0acc4881c870ed493b96
camila <camilamaita07@gmail.com>
2023-06-27 00:38:49 -0300

    querys

--- PORTFOLIO/mov.js
+++ PORTFOLIO/mov.js
@@ -34,15 +34,15 @@ function animation_menu(){
 
 
 // seccion_home.addEventListener('mouseout', ()=>{
-//     cursor.style.display = 'none';
+// cursor.style.display = 'none';
 // })
 
-// const btn = document.querySelector('.btn2');
+const btn = document.querySelector('.btn2');
 
-// btn.addEventListener('click', () =>{
-//     document.body.classList.toggle('white');
-//     let btn2 =  document.querySelector('.btn2');
-//     btn2.classList.toggle('active');
-//     let me = document.querySelector('#Me');
-//     me.classList.toggle('transicion')
-// });
+btn.addEventListener('click', () =>{
+    document.body.classList.toggle('white');
+    let btn2 =  document.querySelector('.btn2');
+    btn2.classList.toggle('active');
+    let me = document.querySelector('#Me');
+    me.classList.toggle('transicion')
+});
