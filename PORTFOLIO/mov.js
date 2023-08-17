1f7ed391e998ace4f6ec99e516da6d6def4e9291
camila <camilamaita07@gmail.com>
2023-08-16 22:11:03 -0300

    slider

--- PORTFOLIO/mov.js
+++ PORTFOLIO/mov.js
@@ -13,28 +13,20 @@ function animation_menu(){
 }
 
 
+//-------------------CAROUSEL-----------------------//
 
 
 
 
 
 
-// const cursor = document.querySelector('.cursor');
-// const seccion_home = document.querySelector('#Home');
 
 
-// seccion_home.addEventListener('mousemove', (e) => {
-//     let x = e.pageX;
-//     let y = e.pageY;
-//     cursor.style.top = y + 'px';
-//     cursor.style.left = x + 'px';
-//     cursor.style.display = 'block';
-// })
 
 
-// seccion_home.addEventListener('mouseout', ()=>{
-// cursor.style.display = 'none';
-// })
+
+
+
 
 const btn = document.querySelector('.btn2');
 
