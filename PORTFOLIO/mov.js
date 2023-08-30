09119d076306e744a53a2e1d61c8e783ef1a25f5
camila <camilamaita07@gmail.com>
2023-08-30 20:26:36 -0300

    detalles

--- PORTFOLIO/mov.js
+++ PORTFOLIO/mov.js
@@ -13,17 +13,13 @@ function animation_menu(){
 }
 
 
-//-------------------CAROUSEL-----------------------//
-
-
 const btn = document.querySelector('.btn2');
 
 btn.addEventListener('click', () =>{
     document.body.classList.toggle('white');
     let btn2 =  document.querySelector('.btn2');
     btn2.classList.toggle('active');
-    let me = document.querySelector('#Me');
-    me.classList.toggle('transicion')
+
 });
 
 window.addEventListener('scroll', ()=>{
